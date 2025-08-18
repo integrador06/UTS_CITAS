76fec94b8e396678d4af31d4ef0ce5f2e802cba4
Adamaris <adamarisalexiagonzalezviruet@gmail.com>
2025-08-18 09:19:04 -0700

    Pantalla especialista

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace EspecialistaPROYECTO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNUEVO = new System.Windows.Forms.Button();
+            this.buttonGUARDAR = new System.Windows.Forms.Button();
+            this.buttonELIMINAR = new System.Windows.Forms.Button();
+            this.buttonSALIR = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEspecialidad = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonNUEVO
+            // 
+            this.buttonNUEVO.Location = new System.Drawing.Point(59, 370);
+            this.buttonNUEVO.Name = "buttonNUEVO";
+            this.buttonNUEVO.Size = new System.Drawing.Size(129, 34);
+            this.buttonNUEVO.TabIndex = 0;
+            this.buttonNUEVO.Text = "NUEVO";
+            this.buttonNUEVO.UseVisualStyleBackColor = true;
+            this.buttonNUEVO.Click += new System.EventHandler(this.buttonNUEVO_Click);
+            // 
+            // buttonGUARDAR
+            // 
+            this.buttonGUARDAR.Location = new System.Drawing.Point(433, 370);
+            this.buttonGUARDAR.Name = "buttonGUARDAR";
+            this.buttonGUARDAR.Size = new System.Drawing.Size(129, 32);
+            this.buttonGUARDAR.TabIndex = 1;
+            this.buttonGUARDAR.Text = "GUARDAR";
+            this.buttonGUARDAR.UseVisualStyleBackColor = true;
+            this.buttonGUARDAR.Click += new System.EventHandler(this.buttonGUARDAR_Click);
+            // 
+            // buttonELIMINAR
+            // 
+            this.buttonELIMINAR.Location = new System.Drawing.Point(241, 370);
+            this.buttonELIMINAR.Name = "buttonELIMINAR";
+            this.buttonELIMINAR.Size = new System.Drawing.Size(129, 32);
+            this.buttonELIMINAR.TabIndex = 2;
+            this.buttonELIMINAR.Text = "ELIMINAR ";
+            this.buttonELIMINAR.UseVisualStyleBackColor = true;
+            this.buttonELIMINAR.Click += new System.EventHandler(this.buttonELIMINAR_Click);
+            // 
+            // buttonSALIR
+            // 
+            this.buttonSALIR.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonSALIR.Location = new System.Drawing.Point(603, 371);
+            this.buttonSALIR.Name = "buttonSALIR";
+            this.buttonSALIR.Size = new System.Drawing.Size(129, 33);
+            this.buttonSALIR.TabIndex = 3;
+            this.buttonSALIR.Text = "SALIR";
+            this.buttonSALIR.UseVisualStyleBackColor = false;
+            this.buttonSALIR.Click += new System.EventHandler(this.buttonSALIR_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(187, 10);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(202, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtEspecialidad
+            // 
+            this.txtEspecialidad.Location = new System.Drawing.Point(115, 40);
+            this.txtEspecialidad.Name = "txtEspecialidad";
+            this.txtEspecialidad.Size = new System.Drawing.Size(182, 20);
+            this.txtEspecialidad.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EspecialistaPROYECTO.Properties.Resources.descarga__3_;
+            this.pictureBox1.Location = new System.Drawing.Point(296, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(70, 72);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 7;
+            // 
+            // Grid
+            // 
+            this.Grid.AllowUserToAddRows = false;
+            this.Grid.AllowUserToDeleteRows = false;
+            this.Grid.AllowUserToResizeRows = false;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.Grid.Location = new System.Drawing.Point(34, 232);
+            this.Grid.Name = "Grid";
+            this.Grid.RowHeadersVisible = false;
+            this.Grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Grid.Size = new System.Drawing.Size(403, 111);
+            this.Grid.TabIndex = 8;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "id";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "nombre";
+            this.Column1.HeaderText = "NOMBRE";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "especialidad";
+            this.Column2.HeaderText = "ESPECIALIDAD";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "fecha";
+            this.Column3.HeaderText = "FECHA";
+            this.Column3.Name = "Column3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "NOMBRE DEL ESPECIALISTA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ESPECIALIDAD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "FECHA:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dtpFecha);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.txtEspecialidad);
+            this.panel1.Location = new System.Drawing.Point(34, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(630, 113);
+            this.panel1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Grid);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonSALIR);
+            this.Controls.Add(this.buttonELIMINAR);
+            this.Controls.Add(this.buttonGUARDAR);
+            this.Controls.Add(this.buttonNUEVO);
+            this.Name = "Form1";
+            this.Text = "ESPECIALISTA";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNUEVO;
+        private System.Windows.Forms.Button buttonGUARDAR;
+        private System.Windows.Forms.Button buttonELIMINAR;
+        private System.Windows.Forms.Button buttonSALIR;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEspecialidad;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace EspecialistaPROYECTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LlenarGrid();
+        }
+
+
+        string connectionString = "workstation id=generador_de_citas.mssql.somee.com;packet size=4096;user id=uts2025_SQLLogin_1;pwd=ueow787blr;data source=generador_de_citas.mssql.somee.com;persist security info=False;initial catalog=generador_de_citas;TrustServerCertificate=True";
+
+        private void LlenarGrid()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlQuery = "SELECT id, nombre, especialidad, fecha FROM Especialista";
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlQuery, connection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+
+                    Grid.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error {ex.Message}");
+            }
+        }
+
+        
+        private void buttonSALIR_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonELIMINAR_Click(object sender, EventArgs e)
+        {
+            if (Grid.SelectedRows.Count > 0)
+            {
+
+                int idSeleccionado = Convert.ToInt32(Grid.SelectedRows[0].Cells[0].Value);
+
+
+                DialogResult confirmacion = MessageBox.Show(
+                    "¿Estás seguro de que deseas eliminar ?",
+                    "Confirmar Eliminación",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (confirmacion == DialogResult.Yes)
+                {
+                    string connectionString = "workstation id=generador_de_citas.mssql.somee.com;packet size=4096;user id=uts2025_SQLLogin_1;pwd=ueow787blr;data source=generador_de_citas.mssql.somee.com;persist security info=False;initial catalog=generador_de_citas;TrustServerCertificate=True";
+                    string sqlQuery = "DELETE FROM Especialista WHERE id = @id";
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                            {
+                                command.Parameters.AddWithValue("@id", idSeleccionado);
+                                command.ExecuteNonQuery();
+
+                                MessageBox.Show("eliminado correctamente.");
+
+
+                                LlenarGrid();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"NO SE ELIMINO : {ex.Message}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona una fila para eliminar.");
+            }
+        
+
+        }
+
+        void Limpiar()
+        {
+
+            txtNombre.Text = "";
+            txtEspecialidad.Text = "";
+
+
+        }
+        
+
+        private void buttonNUEVO_Click(object sender, EventArgs e)
+        {
+
+            Limpiar();
+        }
+
+        private void buttonGUARDAR_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+
+                    string nombre = txtNombre.Text;
+                    string especialidad = txtEspecialidad.Text;
+                    DateTime fecha = dtpFecha.Value;
+
+                    string sqlQuery = "INSERT INTO Especialista(nombre, especialidad, fecha) VALUES (@nombre, @especialidad, @fecha)";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@nombre", nombre);
+                        command.Parameters.AddWithValue("@especialidad", especialidad);
+                        command.Parameters.AddWithValue("@fecha", fecha);
+
+
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("¡Datos guardados correctamente!");
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ocurrió un error al guardar los datos: {ex.Message}");
+                }
+            }
+        }
+    }
+    
+}
